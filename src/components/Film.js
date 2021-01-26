0092c4bd05f0c26dc0a9ed94ef1f782f3a0c09cc
Mark <markmeldrum70@gmail.com>
2021-01-26 08:56:10 +0000

    Working

---
+++ src/components/Film.js
@@ -0,0 +1,9 @@
+import react from "react";
+const Film = ({title, children}) => {
+    return(
+        <>
+            <h4><a href={children}>{title}</a></h4>
+        </>
+    )
+}
+export default Film;
